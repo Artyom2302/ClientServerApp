d65e0241d5a52dd5ac584a46a135812eab4fd545
Artyom2302 <89930657+Artyom2302@users.noreply.github.com>
2023-09-18 07:17:26 +0300

    Добавьте файлы проекта.

---
+++ SocketClient/SocketClient.cpp
@@ -0,0 +1,116 @@
+// SocketClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../SocketServer/Message.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+
+int EnterInt(int downBorder = 0, int upBorder = 1000) {
+	int value;
+	cin >> value;
+	while (cin.fail() && value >= downBorder && value <= upBorder) {
+		cin.clear();
+		cin.ignore('\n');
+		cout << "Enter again:" << endl;
+		cin >> value;
+	};
+	return value;
+}
+
+void MenuHandler() {
+	cout << endl << "Enter client Id address(-1 to stop chat):";
+	int adr = EnterInt(-1);
+	if (adr == -1) {
+		Message::send(MR_BROKER, MT_EXIT);
+	}
+	cout << endl << "Enter msg: ";
+	string str;
+	cin >> str;
+	Message::send(adr == 0 ? MR_ALL : adr + 100, MT_DATA, str);
+	cout << endl << "Continue(1) or No(2)";
+	adr = EnterInt(1, 2);
+
+}
+
+void ProcessMessages()
+{
+	while (true)
+	{
+		Message m = Message::send(MR_BROKER, MT_GETDATA);
+		switch (m.header.type)
+		{
+		case MT_DATA:
+			cout <<endl<<"New message: "<< m.data << endl;
+			MenuHandler();
+			break;
+		case MT_NOT_FOUND:
+			cout << "Client not found\n";
+			break;
+		case MT_CONFIRM:{
+			cout << "Exit"<<exit;
+			break;
+		}
+		case MT_NODATA: {
+			Sleep(500);
+		}
+		default:
+			Sleep(500);
+			break;
+		}
+	}
+}
+
+
+
+
+void Client()
+{
+	AfxSocketInit();
+	thread t(ProcessMessages);
+	t.detach();
+
+	Message m = Message::send(MR_BROKER, MT_INIT);
+
+	while (true)
+	{
+		MenuHandler();
+	}
+}
+
+CWinApp theApp;
+
+int main()
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(nullptr);
+
+	if (hModule != nullptr)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+		{
+			// TODO: code your application's behavior here.
+			wprintf(L"Fatal Error: MFC initialization failed\n");
+			nRetCode = 1;
+		}
+		else
+		{
+			Client();
+		}
+	}
+	else
+	{
+		// TODO: change error code to suit your needs
+		wprintf(L"Fatal Error: GetModuleHandle failed\n");
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
---
+++ SocketServer/Session.h
@@ -0,0 +1,39 @@
+#pragma once
+
+class Session
+{
+public:
+	int id;
+	string name;
+	queue<Message> messages;
+	time_t lastConnectionTime;
+
+	CCriticalSection cs;
+	Session(int _id, string _name)
+		:id(_id), name(_name), lastConnectionTime(time(NULL))
+	{
+
+	}
+
+	void add(Message& m)
+	{
+		CSingleLock lock(&cs, TRUE);
+		messages.push(m);
+		lastConnectionTime = time(NULL);
+	}
+
+	void send(CSocket& s)
+	{
+		CSingleLock lock(&cs, TRUE);
+		if (messages.empty())
+		{
+			Message::send(s, id, MR_BROKER,MT_NODATA);	
+		}
+		else
+		{
+			messages.front().send(s);
+			messages.pop();
+		}
+	}
+};
+
